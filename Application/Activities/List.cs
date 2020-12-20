168f59dd96d4fd738eba18fdc992e2dbd6d4ff0f
Derrick <derrick.xiang@gmail.com>
2020-12-20 09:13:09 +0800

    End of module 4

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -1,4 +1,3 @@
-
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,14 +18,13 @@ namespace Application.Activities
         public class Handler: IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
-
             public Handler(DataContext context) 
             {
                 _context = context;
             }
 
             public async Task<List<Activity>> Handle(Query request, 
-                CancellationToken cancellation)
+                CancellationToken cancellationToken)
                 {
                     var activities = await _context.Activities.ToListAsync();
 
